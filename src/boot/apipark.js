d502ed70c645aaf77a5eaec3b017a5e2af1bfe7c
lalokob <lalo.kob@gmail.com>
2020-08-06 20:39:49 -0500

    se ajusta la ip para produccion

--- src/boot/apipark.js
+++ src/boot/apipark.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import axios from 'axios'
 
 const apipark = axios.create({
-    baseURL: 'http://192.168.1.243/test/smartparkapi/public',//testing
-    // baseURL: 'http://192.168.1.243/smartparkapi/public',//prod
+    // baseURL: 'http://192.168.1.243/test/smartparkapi/public',//testing
+    baseURL: 'http://192.168.1.243/smartparkapi/public',//prod
     // baseURL: 'http://192.168.1.86/smartpark/public',//dev
     timeout:2500,
 });
