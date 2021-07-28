17292729d19b76584c54d1a9185587c2141c9b5d
lalokob <lalo.kob@gmail.com>
2021-07-27 22:53:12 -0500

    construccion para servidor nuevo

--- src/boot/apipark.js
+++ src/boot/apipark.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import axios from 'axios'
 
 const apipark = axios.create({
-    // baseURL: 'http://192.168.10.4/smartparkapi/public',//prod
-    baseURL: 'http://192.168.10.15/smartparkapi/public',//dev
-    timeout: 20000,
+    baseURL: 'http://192.168.10.12/smartparkapi/public',//prod
+    // baseURL: 'http://192.168.10.15/smartparkapi/public',//dev
+    timeout: 8000,
 });
 
 // para usar dentro de componentes
